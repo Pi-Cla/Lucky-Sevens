6ba2414636ca24b0ff07091dbe7f0771a20d954a
Pi-Cla <pirateclip@protonmail.com>
2019-02-19 13:34:40 -0700

    Project Completed

--- Lucky-Sevens/Program.cs
+++ Lucky-Sevens/Program.cs
@@ -6,7 +6,23 @@ namespace Lucky_Sevens
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int totalNumberOfSevens = 0;
+            Random rng = new Random();
+
+            int die1;
+            int die2;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                die1 = rng.Next(1, 7);
+                die2 = rng.Next(1, 7);
+
+                if(die1 + die2 == 7){
+                    totalNumberOfSevens++;
+                }
+            }
+            Console.WriteLine("Out of 100 rolls, you rolled {0} sevens", totalNumberOfSevens);
+            Console.ReadLine();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lucky-Sevens
+Full Name: Gordon Kai Ching Leung<br>
+Student ID: 147547<br>
+Date Completed: 19/02/2019<br>
+Run this with 'dotnet run'.
