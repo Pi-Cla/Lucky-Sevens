14697091e366e72f4d863c6778e5f2d74ac78a0c
Pi-Cla <pirateclip@protonmail.com>
2019-02-19 13:23:44 -0700

    files for console app

---
+++ Lucky-Sevens/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lucky_Sevens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
